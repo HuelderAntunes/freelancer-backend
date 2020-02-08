1298c7ebf2d1e83b5588a39d87de905d0d94df9e
Huelder <huelderantuness@gmail.com>
2020-02-08 20:58:44 -0300

    add: initial files

---
+++ freelancer/accounts/admin.py
@@ -0,0 +1,12 @@
+from django.contrib import admin
+from .models import *
+
+admin.site.register(Role)
+admin.site.register(BankAccount)
+admin.site.register(PersonalData)
+admin.site.register(AvaliationField)
+admin.site.register(AvaliationValue)
+admin.site.register(Avaliation)
+admin.site.register(File)
+admin.site.register(CustomField)
+admin.site.register(CustomFieldValue)
---
+++ freelancer/accounts/models.py
@@ -0,0 +1,99 @@
+from django.db.models import *
+from django.contrib.auth.admin import User
+
+
+class Role(Model):
+    name = CharField(null=False, blank=False, max_length=50)
+    description = CharField(max_length=255)
+
+    def __str__(self):
+        return f"{self.naAvaliationme} role"
+
+class File(Model):
+    name = CharField(max_length=50)
+    path = URLField(null=False, blank=False)
+    created_at = DateField(auto_now_add=True)
+    updated_at = DateField(auto_now=True)
+
+    def __str__(self):
+        return f"{self.name} file"
+
+class BankAccount(Model):
+    user = ForeignKey(User, on_delete=CASCADE)
+    cpf = CharField(max_length=20)
+    holder = CharField(max_length=130)
+    account = CharField(max_length=20)
+    agence = CharField(max_length=10)
+    bank = CharField(max_length=10)
+
+    def __str__(self):
+        return f"{self.bank} - {self.user} bank account"
+
+class PersonalData(Model):
+    user = ForeignKey(User, on_delete=CASCADE)
+    role = ForeignKey(Role, on_delete=SET_NULL, null=True)
+    avatar = ForeignKey(File, on_delete=SET_NULL, null=True)
+    birth_date = DateField()
+    cpf = CharField(max_length=20)
+    cnpj = CharField(max_length=50)
+    rg = CharField(max_length=20)
+    address = CharField(max_length=255)
+    address_complement = CharField(max_length=255)
+    state = CharField(max_length=30)
+    country = CharField(max_length=30)
+    state_subscription = CharField(max_length=100)
+    civic_subscription = CharField(max_length=100)
+    phone = CharField(max_length=20)
+    comercial_email = EmailField()
+
+    def __str__(self):
+        return f"{self.user.username} personal data"
+
+
+class CustomField(Model):
+    name = CharField(null=False, blank=False, max_length=50)
+    description = CharField(max_length=255)
+    data_type = CharField(max_length=30)
+
+    def __str__(self):
+        return f"{self.name} customfield"
+
+
+class CustomFieldValue(Model):
+    custom_field = ForeignKey(CustomField, on_delete=SET_NULL, null=True)
+    user = ForeignKey(User, on_delete=CASCADE)
+    date_value = DateTimeField()
+    text_value = CharField(max_length=255)
+    number_value = FloatField()
+    created_at = DateField(auto_now_add=True)
+    updated_at = DateField(auto_now=True)
+
+    def __str__(self):
+        return f"{self.custom_field.name} of {self.user.username}"
+
+
+class Avaliation(Model):
+    evaluator = ForeignKey(User, on_delete=CASCADE, related_name="evaluator_user")
+    rated = ForeignKey(User, on_delete=CASCADE, related_name="rated_user")
+    created_at = DateField(auto_now_add=True)
+    updated_at = DateField(auto_now=True)
+
+    def __str__(self):
+        return f"{self.rated} avaliation by {self.evaluator}"
+
+
+class AvaliationField(Model):
+    name = CharField(null=False, blank=False, max_length=50)
+    description = CharField(max_length=255)
+
+    def __str__(self):
+        return f"{self.name} avaliation field"
+
+
+class AvaliationValue(Model):
+    avaliation = ForeignKey(Avaliation, on_delete=CASCADE)
+    avaliation_type = ForeignKey(AvaliationField, on_delete=CASCADE)
+    rank = IntegerField(null=False, blank=False)
+
+    def __str__(self):
+        return f"{self.avaliation_type} of {self.avaliation.rated}"
