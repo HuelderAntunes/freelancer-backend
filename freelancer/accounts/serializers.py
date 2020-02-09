079172a8dd9fe9d61371d142ae17f4dde6323337
FahRamatis <fah.ramatis@gmail.com>
2020-02-09 19:45:49 -0300

    add: serializers

--- freelancer/accounts/serializers.py
+++ freelancer/accounts/serializers.py
@@ -1,5 +1,5 @@
 from rest_framework import serializers
-from .models import ForgotPassword
+from .models import ForgotPassword, PersonalData, BankAccount, File
 
 
 class ForgotPasswordSerializer(serializers.Serializer):
@@ -10,3 +10,27 @@ class RecoverPasswordSerializer(serializers.Serializer):
     email = serializers.EmailField()
     token = serializers.CharField()
     new_password = serializers.CharField()
+
+
+class PersonalDataSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = PersonalData
+        fields = '__all__'
+
+
+class BankAccountSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = BankAccount
+        fields = '__all__'
+
+
+class FileSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = File
+        fields = ['name', 'path']
+        read_only_fields = ['created_at', 'updated_at']
+
+
+class RoleSerializer(serializers.Serializer):
+    name = serializers.CharField()
+    description = serializers.CharField()
